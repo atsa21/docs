012b744644af054a59987eee6f03403371b33df3
Anna Sakhaudinova <104850911+atsa21@users.noreply.github.com>
2025-04-20 12:42:25 +0300

    FEAT: add-basic-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'home', component: MainComponent },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- src/app/pages/main/pages/reviewer/reviewer.component.ts
+++ src/app/pages/main/pages/reviewer/reviewer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reviewer',
+  standalone: true,
   imports: [],
   templateUrl: './reviewer.component.html',
   styleUrl: './reviewer.component.scss'
